70dbedc596e47a14f501dc3c1fb427651419f2aa
vanshika898 <vanshikanigam8989@gmail.com>
2024-05-17 19:16:44 +0530

    100-days-code-challenge

--- exeHandling/q1.java
+++ exeHandling/q1.java
@@ -7,16 +7,15 @@ public class q1 {
         x = 20;
         y = 10;
         int arr[] = new int[5];
-        try {
-            try {
-                int z = x / y;
-                System.out.println(z);
-                arr[300] = z;
-                System.out.println(arr[z]);
 
-            } catch (ArithmeticException ae) {
-                System.out.println("Exception: Do not divide with zero");
-            }
+        try {
+            int z = x / y;
+            System.out.println(z);
+            arr[300] = z;
+            System.out.println(arr[z]);
+
+        } catch (ArithmeticException ae) {
+            System.out.println("Exception: Do not divide with zero");
         } catch (IndexOutOfBoundsException k) {
             System.out.println("Exception: index out of bounds ");
 
