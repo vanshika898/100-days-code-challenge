9100bd1509157254f0e3c2a37389aa13c95b2a66
vanshika898 <vanshikanigam8989@gmail.com>
2024-05-17 19:14:31 +0530

    100-days-code-chakkenge

---
+++ exeHandling/q1.java
@@ -0,0 +1,27 @@
+// Execption handling (Simple question)
+
+public class q1 {
+    public static void main(String[] args) {
+
+        int x, y;
+        x = 20;
+        y = 10;
+        int arr[] = new int[5];
+        try {
+            try {
+                int z = x / y;
+                System.out.println(z);
+                arr[300] = z;
+                System.out.println(arr[z]);
+
+            } catch (ArithmeticException ae) {
+                System.out.println("Exception: Do not divide with zero");
+            }
+        } catch (IndexOutOfBoundsException k) {
+            System.out.println("Exception: index out of bounds ");
+
+        }
+
+    }
+
+}
