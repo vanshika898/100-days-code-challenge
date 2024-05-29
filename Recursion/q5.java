22753192afc35aefff7c80d2bee80a8d6c623d74
vanshika898 <vanshikanigam8989@gmail.com>
2024-05-29 23:21:17 +0530

    100-days-code-challenge

---
+++ Recursion/q5.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class q5 {
+    public static int fibo(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("fibonacci number is:" + fibo(5));
+
+    }
+
+}
